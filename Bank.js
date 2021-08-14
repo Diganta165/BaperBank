301532ba3d16422b8ebeb01fb9220ae5a0ad6cf9
Diganta165 <mdsrdiganta@gmail.com>
2021-08-15 05:13:32 +0600

    Added functionalities on Login Form and redirect to another page after clicking on the submit button

---
+++ Bank.js
@@ -0,0 +1,14 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    //catching input email value
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    
+    //catching input password value
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    //validation check
+    if(userEmail == "baperbank@omuktomuk.com" && userPassword == "amarbank"){
+        window.location.href = 'Banking.html';
+    }
+})
