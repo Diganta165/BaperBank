5b48cd1f1347c2e31c1040413a76437fe425d7e8
Diganta165 <mdsrdiganta@gmail.com>
2021-08-16 05:28:33 +0600

    Added withdraw process

--- Banking.js
+++ Banking.js
@@ -29,3 +29,32 @@ document.getElementById('deposit-submit').addEventListener('click', function(){
 
     depositField.value = '';
 })
+
+//withdraw
+
+document.getElementById('withdraw-submit').addEventListener('click',function(){
+    const withdrawField = document.getElementById('withdraw-input');
+    const newWithdrawAmountText = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(withdrawTotalText);
+
+    const newWithdrawTotal = newWithdrawAmount + previousWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update account balance 
+    const balanceTotal = document.getElementById('balance-total');
+
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotalAmount - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear withdraw input 
+    withdrawField.value ='';
+
+})
